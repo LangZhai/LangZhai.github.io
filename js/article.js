bb8247128fa7810b71907bc340f08c2bfd83d148
LangZhai <zl2012xyz@hotmail.com>
2016-04-19 17:16:55 +0800

    Daily update

---
+++ js/article.js
@@ -0,0 +1,14 @@
+$(function () {
+    $.ajax({
+        url: 'data.json',
+        dataType: 'json'
+    }).done(function (data) {
+        $('body>article').html($('#template_article').template($.extend({
+            escape: function (html) {
+                return html.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+            }
+        }, data)));
+        eval(data.code);
+        Prism.highlightAll();
+    });
+});
