fbd5c6257c3e5e2757b229bad85a506d57e5516b
LangZhai <zl2012xyz@hotmail.com>
2016-09-06 19:14:18 +0800

    Daily update

--- js/article.js
+++ js/article.js
@@ -6,11 +6,7 @@ $(function () {
         url: 'http://langzhai.github.io' + url.substring(url.lastIndexOf('/')) + '/data.json',
         dataType: 'json'
     }).done(function (data) {
-        $('body>article').html($('#template_article').template($.extend({
-            escape: function (html) {
-                return html.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
-            }
-        }, data)));
+        $('body>article').html($('#template_article').template(data));
         eval(data.code);
         Prism.highlightAll();
     });
