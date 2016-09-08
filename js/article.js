7371ecd4e2e39d203c9502821c416a4caf429630
LangZhai <zl2012xyz@hotmail.com>
2016-09-08 10:45:34 +0800

    Daily update

--- js/article.js
+++ js/article.js
@@ -3,7 +3,7 @@ $(function () {
 
     $.support.cors = true;
     $.ajax({
-        url: (url.match(/^http/) ? url.substring(0, url.indexOf('//') + 2) : 'https://') + 'langzhai.github.io' + url.substring(url.lastIndexOf('/')) + '/data.json',
+        url: (url.match(/^http/) ? url.substring(0, url.indexOf('//')) : 'https:') + '//langzhai.github.io' + url.substring(url.lastIndexOf('/')) + '/data.json',
         dataType: 'json'
     }).done(function (data) {
         $('body>article').html($('#template_article').template(data));
