26ebae80180061c380582f50601881506a6c0458
LangZhai <zl2012xyz@hotmail.com>
2016-09-08 13:33:16 +0800

    Daily update

--- js/article.js
+++ js/article.js
@@ -1,7 +1,7 @@
 $(function () {
     $.support.cors = true;
     $.ajax({
-        url: (location.protocol.match(/^http/) ? location.protocol : 'https:') + '//langzhai.github.io' + location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/data.json',
+        url: 'https://langzhai.github.io' + location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/data.json',
         dataType: 'json'
     }).done(function (data) {
         $('body>article').html($('#template_article').template(data));
