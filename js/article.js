37e6aed3f299bd4df9afc3146e026126bb282c8e
智能小菜菜 <zl2012xyz@hotmail.com>
2017-05-17 18:01:07 +0800

    Daily update

--- js/article.js
+++ js/article.js
@@ -1,17 +1,20 @@
-$(function () {
-    $.support.cors = true;
-    $.ajax({
-        url: (location.protocol !== 'file:' || typeof remoteUrl === 'undefined' ? '' : remoteUrl) + 'data.json',
-        dataType: 'json'
-    }).done(function (data) {
-        document.querySelector('body>article').innerHTML = ZLTemplate('#template_article').template(data);
-        if (data.code instanceof Array) {
-            data.code.forEach(function (item) {
-                eval(item);
-            });
-        } else {
-            eval(data.code);
+(function (xmlHttp) {
+    xmlHttp.open('GET', (location.protocol !== 'file:' || typeof remoteUrl === 'undefined' ? '' : remoteUrl) + 'data.json', true);
+    xmlHttp.send(null);
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            (function (data) {
+                data = JSON.parse(data);
+                document.querySelector('body>article').innerHTML = ZLTemplate('#template_article').template(data);
+                if (data.code instanceof Array) {
+                    data.code.forEach(function (item) {
+                        eval(item);
+                    });
+                } else {
+                    eval(data.code);
+                }
+                Prism.highlightAll();
+            }(xmlHttp.responseText));
         }
-        Prism.highlightAll();
-    });
-});
+    };
+}(XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')));
