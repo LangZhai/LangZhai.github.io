87fae3b4dd88762921fe62a56e41f5dd57b30197
智能小菜菜 <zl2012xyz@hotmail.com>
2017-05-21 22:28:41 +0800

    Daily update

--- blog/js/list.js
+++ blog/js/list.js
@@ -1,12 +1,9 @@
 (function (xmlHttp) {
-    var template_list = document.querySelector('#template_list'),
-        list = document.querySelector('#list');
-
     xmlHttp.open('GET', 'blog.json', true);
     xmlHttp.send(null);
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            list.innerHTML = template_list.template(JSON.parse(xmlHttp.responseText));
+            document.querySelector('#list').innerHTML = ZLTemplate('#template_list').template(JSON.parse(xmlHttp.responseText));
         }
     };
 }(XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')));
